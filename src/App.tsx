f515e612e512963d1f8384b32b03c60c9c0d45e2
陈浩（Hao Chen） <3418085891@qq.com>
2026-06-20 15:13:43 +0800

    Set placeholder again

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createElement } from 'react';
 
 export default function App() {
-  return createElement('div', { style: { color: 'white', padding: 40 } }, 'Romance Orbit');
+  return createElement('div', { style: { color: 'white', padding: 40 } }, 'Aurora Rose Orbit');
 }
