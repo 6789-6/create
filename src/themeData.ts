0d74bd799871a50fdff328770b27996375c28fa7
陈浩（Hao Chen） <3418085891@qq.com>
2026-06-20 15:06:58 +0800

    Add theme data

---
+++ src/themeData.ts
@@ -0,0 +1,5 @@
+export const themeItems = [
+  { id: 'aurora', title: '极光', color: '#91fff0' },
+  { id: 'rose', title: '玫瑰', color: '#ff8ac7' },
+  { id: 'moon', title: '月光', color: '#fff0b8' }
+];
